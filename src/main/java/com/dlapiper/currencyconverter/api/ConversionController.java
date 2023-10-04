587c8e8d87f2612b1cbd275c648a2c0c8f4593e1
thalita <t.vergilio@leedsbeckett.ac.uk>
2023-10-04 20:04:55 +0100

    Added parameter validation for API endpoint. Added integration tests.

--- src/main/java/com/dlapiper/currencyconverter/api/ConversionController.java
+++ src/main/java/com/dlapiper/currencyconverter/api/ConversionController.java
@@ -3,7 +3,12 @@ package com.dlapiper.currencyconverter.api;
 import com.dlapiper.currencyconverter.ConversionService;
 import com.dlapiper.currencyconverter.model.Conversion;
 import com.dlapiper.currencyconverter.model.ConversionModelAssembler;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +24,15 @@ public class ConversionController {
     }
 
     @PostMapping("/api/convert")
-    public EntityModel<Conversion> convert(String source,
-                                           String target,
-                                           Double amount) {
+    public EntityModel<Conversion> convert(@NotNull @NotEmpty String source,
+                                           @NotNull @NotEmpty String target,
+                                           @NotNull @NotEmpty Double amount) {
         var result = conversionService.convertCurrency(source, target, amount);
         return assembler.toModel(new Conversion(source, target, amount, result));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
---
+++ src/test/java/com/dlapiper/currencyconverter/api/ConversionControllerIntegrationTest.java
@@ -0,0 +1,96 @@
+package com.dlapiper.currencyconverter.api;
+
+import com.dlapiper.currencyconverter.ConversionService;
+import com.dlapiper.currencyconverter.model.ConversionModelAssembler;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ConversionControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ConversionService conversionService;
+
+    @Autowired
+    private ConversionModelAssembler assembler;
+
+    // Test cases
+
+    @Test
+    void testConvertEndpointWithValidInput() throws Exception {
+        // Arrange
+        var source = "USD";
+        var target = "EUR";
+        double amount = 100.0;
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/convert")
+                        .param("source", source)
+                        .param("target", target)
+                        .param("amount", String.valueOf(amount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.source").value(source))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.target").value(target))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount").value(amount))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").isNotEmpty());
+    }
+
+    @Test
+    void testConvertEndpointWithInvalidSourceCurrency() throws Exception {
+        // Arrange
+        var source = "INVALID";
+        var target = "EUR";
+        double amount = 100.0;
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/convert")
+                        .param("source", source)
+                        .param("target", target)
+                        .param("amount", String.valueOf(amount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void testConvertEndpointWithInvalidTargetCurrency() throws Exception {
+        // Arrange
+        var source = "USD";
+        var target = "INVALID";
+        double amount = 100.0;
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/convert")
+                        .param("source", source)
+                        .param("target", target)
+                        .param("amount", String.valueOf(amount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void testConvertEndpointWithNegativeAmount() throws Exception {
+        // Arrange
+        var source = "USD";
+        var target = "EUR";
+        double amount = -100.0;
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/convert")
+                        .param("source", source)
+                        .param("target", target)
+                        .param("amount", String.valueOf(amount))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
